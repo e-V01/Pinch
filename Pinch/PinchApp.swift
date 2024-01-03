9b44df4d0f54e0fd34418ac5f0d46a0fb601dde3
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-03 13:18:46 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Y K on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
