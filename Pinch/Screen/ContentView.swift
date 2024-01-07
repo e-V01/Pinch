2cbe5a4ed2591a7654f3b43517c6e753b0473baa
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-07 14:19:32 +0200

    Control View at the bottom was added and the size of object can be easily resized in 3 different variations

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
-
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -47,17 +47,17 @@ struct ContentView: View {
                     })
                 // MARK: DragGesture
                     .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            withAnimation(.linear(duration: 1)) {
-                                imageOffset = value.translation
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
                             }
-                        }
-                        .onEnded { _ in
-                            if imageScale <= 1 {
-                                resetImageState()
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
                             }
-                        }
                     )
             } // : Zstack
             .navigationTitle("Pinch & Zoom")
@@ -73,6 +73,55 @@ struct ContentView: View {
                     .padding(.horizontal)
                     .padding(.top, 30)
                 , alignment: .top
+            )
+            // MARK: Controls
+            .overlay(
+                Group {
+                    HStack {
+                        // SCALE DOWN
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                        // to prevent unwanted gitches
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        // RESET
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        // SCALE UP
+                        Button {
+                            withAnimation {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                        //safety precaution
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    } // : Controls
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                }
+                    .padding(.bottom, 30),
+                alignment: .bottom
             )
         } // : NavigationV
         .navigationViewStyle(.stack)
