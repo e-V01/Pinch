79d20e82274ac8ae46b2e38e5000cb570673ffb5
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-07 13:25:01 +0200

    draggestore with on change and on ended modified

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    //MARK: Prop
+    //MARK: PROP
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    //MARK: FUNC
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+
     var body: some View {
         NavigationView {
             ZStack {
@@ -22,6 +31,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 // MARK: Tap Gesture
@@ -31,11 +41,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: DragGesture
+                    .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = value.translation
+                            }
+                        }
+                        .onEnded { _ in
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                        }
+                    )
             } // : Zstack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
